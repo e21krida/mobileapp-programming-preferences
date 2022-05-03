af9bef2621ca643a0324712dabd09d8403f6d1c7
e21krida <e21krida@student.his.se>
2022-05-03 14:38:33 +0200

    skapade en onClick tillhörande knappen "save" som presenterar namnet som fylls i samt sparar denna till preferences

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.project;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +12,10 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView TextViewName;
+    EditText username;
+    TextView namepresent;
     SharedPreferences preferences;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +24,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TextViewName = findViewById(R.id.name);
+        username = findViewById(R.id.edit_text);
+        button = findViewById(R.id.save);
+        namepresent = findViewById(R.id.namepresent);
 
         preferences = getSharedPreferences(("preferences"), MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("name", "Krille");
-        editor.apply();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("name", username.getText().toString());
+                editor.apply();
+                namepresent.setText(username.getText().toString());
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
         String name = preferences.getString("name", "inget namn hittades");
-        TextViewName.setText(name);
+        namepresent.setText(name);
     }
 }
